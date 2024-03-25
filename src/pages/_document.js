73f02ad17aa8626158e7658e7162f140beea4286
Brandon Guo <brandonguo@Brandons-MacBook-Air.local>
2024-03-25 17:31:56 -0600

    initial commit

---
+++ src/pages/_document.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link href="/favicon.ico" rel="shortcut icon" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+
+          {/* Uncomment to add favicons for other platforms */}
+          {/* These files can be generated with realfavicongenerator.net */}
+          {/*
+          <link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+          <link href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png"/>
+          <link href="/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+          <link href="/safari-pinned-tab.svg" rel="mask-icon" color="#4a9885" />
+          <link href="/site.webmanifest" rel="manifest" />
+          */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    // Default project breakpoints
+    // https://tailwindcss.com/docs/screens
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
+};
